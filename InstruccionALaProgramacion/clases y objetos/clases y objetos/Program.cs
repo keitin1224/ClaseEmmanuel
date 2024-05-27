c26b8cd54874e994404f56b440a93e8f4e522901
Luisa Mateo <keitin1224@gmail.com>
2024-05-27 19:31:41 -0400

    parte 1 del ejercicio

--- InstruccionALaProgramacion/clases y objetos/clases y objetos/Program.cs
+++ InstruccionALaProgramacion/clases y objetos/clases y objetos/Program.cs
@@ -1,8 +1,51 @@
-﻿const Comida =
-{
-id: 1,
-    nombre: "Manzana",
-color: "rojo"
-};
-consoloe.log(comida.nombre);
+﻿/*  Ejercicios Básicos
+    1. Clases y Objetos :
+    Ejercicio : Crea una clase Persona que tenga atributos nombre, edad y dirección. 
+    Incluye un método para mostrar la información de la persona en consola.
+    Objetivo : Comprender la creación de clases y objetos.
+*/
+/*
+    2. Métodos :
+    Ejercicio : Extiende la clase Persona para incluir un método que calcule 
+    el año de nacimiento de la persona basado en su edad.
+    Objetivo : Aprender a definir y utilizar métodos dentro de una clase.
+*/
+/*
+    3. Herencia :
+    Ejercicio : Crea una clase Empleado que herede de la clase Persona e 
+    incluya atributos adicionales como puesto y salario. Implementa un método 
+    para mostrar toda la información del empleado en consola.
+    Objetivo : Aprender sobre herencia y cómo extender clases existentes.
+*/
+/*
+    4. Polimorfismo :
+    Ejercicio : Crea una clase Animal con un método hacerSonido(). 
+    Luego, crea clases derivadas como Perro y Gato que sobreescriban 
+    el método hacerSonido() para proporcionar implementaciones específicas.
+    Objetivo : Comprender el polimorfismo y la capacidad de las clases derivadas para modificar comportamientos.
+*/
 
+Persona datos = new Persona(33, "Luisa", "Los fraile");
+
+datos.MostrarInformacion();
+
+public class Persona
+{
+    private int edad;
+    private string nombre;
+    private string direccion;
+
+    public Persona(int edad, string nombre, string direccion)
+    {
+        this.edad = edad;
+        this.nombre = nombre;
+        this.direccion = direccion;
+    }
+
+    public void MostrarInformacion()
+    {
+        Console.WriteLine("Nombre \t\t" + nombre);
+        Console.WriteLine("Edad \t\t" + edad);
+        Console.WriteLine("Direccion \t" + direccion);
+    }
+}
--- InstruccionALaProgramacion/clases y objetos/clases y objetos/obj/Debug/net8.0/clases y objetos.AssemblyInfo.cs
+++ InstruccionALaProgramacion/clases y objetos/clases y objetos/obj/Debug/net8.0/clases y objetos.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("clases y objetos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c19c18e02d14b25e34c7c03813dcec140c0a82d0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39f51e689de23e43683866e17e4d36e5d2eb4d4a")]
 [assembly: System.Reflection.AssemblyProductAttribute("clases y objetos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("clases y objetos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
